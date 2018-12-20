10b3ebce6bc703e31c8025f38f8d5cb67ba0dbaf
dapeng <2406753540@qq.com>
2018-12-20 13:50:59 +0800

    天楹相关信息

---
+++ tyhs.sql
@@ -0,0 +1,76 @@
+mysql  C:\wamp64\bin\mysql\mysql5.7.14\bin
+
+
+（1）git账号
+账号:
+AuthUser => guestUser
+AuthPass => shtydev001
+
+（2）git仓库
+断舍狸API：http://guestUser:shtydev001@git.58xiaoxiang.top/ty-recycle-2c.git
+
+（3）B端天楹回收仓库地址
+http://git.58xiaoxiang.top/repo.git
+
+
+（4）API参考文档
+http://api.58xiaoxiang.top/ApiManager/index.php
+
+AuthUserName => admin
+AuthPassword => shtyapidoc123
+
+（5）断舍狸测试前端首页（微信开发工具）
+http://test.58xiaoxiang.com/web/mobile/view/WeChatRecycle/index.html
+天楹回收测试前端首页（微信开发工具）
+http://test.58xiaoxiang.top/web/mobile/view/Index/index.html  18016064185 111111
+
+（6）断舍狸测试接口域名
+test.58xiaoxiang.cn
+
+（7）断舍狸生产环境域名
+www.58xiaoxiang.cn
+
+（8）断舍狸API接口框架Thinkphp5
+
+（9）测试环境数据库
+HOST：msc01beta.58xiaoxiang.cn   3306
+账号：root
+密码：#@!CBA321cba
+
+断舍狸/天楹回收 后台
+http://test.58xiaoxiang.top/web/bg/view/index.html
+18930404499   w12345678
+18016064185   a12345678
+15600000888   w12345678
+preview
+18930404490   w12345678
+断舍狸仓库：ty_duansheli  天楹回收：sh-tb
+
+断舍离FTP
+/usr/local/webserver/www/dev_shtc
+dev.58xiaoxiang.cn
+ip: 139.196.185.136
+账号：www
+密码：betawww
+
+本地虚拟机
+192.168.227.128
+
+天楹回收小程序
+appid wx380db3e67aa97de8
+ 32f426532cf7b8b608817585b9318c68
+
+搬瓦工
+45.62.113.91
+root TLG9Ylwju9Mu
+ssh port 26889
+
+百度bch
+FTP地址： web13383913120.ftp-gz01.bcehost.com 端口：8010
+FTP账号： web13383913120 密码：123456
+
+数据库名称： EFBHHlVorlYVselNxBtj
+连接地址： sqld-gz.bcehost.com
+端口： 3306
+用户名： b6e95f2dba2f43e5b77579012d9393a5
+密码：b95e6d2f283d4159b9158ea13d530a9c
